82ad01eeb6fc10485e0d631fb1e532ff27b6e547
Blair Wishom <blwishom@gmail.com>
2023-04-25 17:01:00 -0700

    added inputs to db

--- mySQL_Queries/little_league.sql
+++ mySQL_Queries/little_league.sql
@@ -1,4 +1,5 @@
--- CREATE LITTLE LEAGUE DB AND TABLES
+
+-- ***************************CREATE LITTLE LEAGUE DB AND TABLES***************************
 
 --CREATE DATABASE if not exists little_league;
 -- USE little_league;
@@ -88,3 +89,19 @@
 -- email VARCHAR(255),
 -- PRIMARY KEY (id)
 -- );
+
+
+-- ***************************************INSERTS***************************************
+-- insert into address values(1, '123 Wallace Ln', 'Polk', 'California', '54321');
+-- insert into address values(2, '45 Renassaince Way', 'Bank', 'California', '12345');
+-- insert into coach values(1, 'Willie', 'Beamin', 3334445555, 'willieb@gmail.com');
+-- insert into coach values(2, 'Archie', 'Bunker', 2221113333, 'archie.bunker@gmail.com');
+-- insert into guardian values(1, 'John', 'Johnson', 5554443333, 'jjohnson@email.com');
+-- insert into guardian values(2, 'Wendy', 'Jackson', 123456789, 'wendy.jackson@gmail.com');
+-- insert into player values(1, 'John', 'Johnson Jr.', '2005-03-11', 5, 1, 1);
+-- insert into player values(2, 'Paul', 'Wall', '2005-05-19', 3, 1, 1);
+-- insert into school values(1, 1);
+-- insert into season values(1, '2023-08-15', '2023-11-28');
+-- insert into season_team values(1, 1, 1, 1);
+-- insert into team values(1, 'East Bay Jaguars');
+-- insert into team values(2, 'East Bay 49ers');
