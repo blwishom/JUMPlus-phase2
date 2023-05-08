ed8d17b43476edd20189d8127cc5ea3e72e41c26
Blair Wishom <blwishom@gmail.com>
2023-05-07 19:28:02 -0700

    lab question

--- unix_practice/forLoop.sh
+++ unix_practice/forLoop.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-names=('Joe' 'Rob' 'Kim' 'Pam' 'Fad')
+# names=('Joe' 'Rob' 'Kim' 'Pam' 'Fad')
+# echo Names:
+# for name in "${names[@]}"; do
+# 	echo $name
+# done
 
-for name in "${names[@]}"; do
-	echo $name
-done
+# echo Names: ${names[@]}
 
-echo Names: ${names[@]}
+# echo Numbers:
+# numbers=(1 2 3 4 5 6 7 8 9 0)
+
+# for num in ${numbers[@]}; do
+# 	echo $num
+# done
+
+
+fruits=(apple strawberry orange pineapple lemon)
+
+echo Fruits:
+
+for fruit in ${fruits[@]}; do
+	echo $fruit
+done | sort
