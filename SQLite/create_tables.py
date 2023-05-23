622b0a09a436b81675d35b9470f6d39ceaf4c36f
Blair Wishom <blwishom@gmail.com>
2023-05-22 20:20:23 -0700

    sqlite

--- SQLite/create_tables.py
+++ SQLite/create_tables.py
@@ -1,5 +1,15 @@
-from connection import create_connection
+# from connection import create_connection
 
 
-def create_table(conn):
-    pass
+# def create_table(conn):
+#     pass
+
+
+dictionary = {
+    'key': 1,
+    'key2': 2
+}
+
+dictionary['key3'] = 3
+
+print(dictionary)
